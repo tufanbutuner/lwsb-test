7c9e0dfcbe84c0c8a26c875c0fc0a47737342b70
tufanbutuner <tufanbutuner@gmail.com>
2022-09-20 22:28:21 +0100

    connect to mongodb atlas

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
-const express = require("express");
-require("dotenv").config();
-const { graphqlHTTP } = "express-graphql";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
 const port = process.env.PORT || 8000;
 const app = express();
+app.use(cors());
 
-app.listen(port, console.log(`Listening on port ${port}`));
+app.use("/posts", postRoutes);
+
+const dbConnectionString = process.env.MONGO_CONNECTION_STRING;
+
+mongoose
+  .connect(dbConnectionString)
+  .then(() => app.listen(port, console.log(`Listening on port ${port}`)))
+  .catch((err) => console.log(err.message));
